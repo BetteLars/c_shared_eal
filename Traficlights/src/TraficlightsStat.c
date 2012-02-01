fef408d9f5b3f549df843076d532f5fc50214b48
Lasse <lars_andre@hotmail.com>
2012-02-01 09:35:46 +0100

    Traficstatmashine

---
+++ Traficlights/src/TraficlightsStat.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : Traficlights.c
+ Author      : Lars
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("let drive"); /* prints let drive */
+	return EXIT_SUCCESS;
+}
